4f362f589ff02d0a4cfc1d6d11efa5c9de36f933
naimish33 <naimish.dobariya@techquaint.com>
2024-12-04 14:35:24 +0530

    Modify : Add exceptions

--- Tesco/TescoParser.py
+++ Tesco/TescoParser.py
@@ -3,11 +3,13 @@ def product_parser(html):
     product_data = {}
 
     soup = BeautifulSoup(html, "html.parser")
-
-    page_json = json.loads(
-        soup.select_one('script[type="application/discover+json"]').text
-    )["mfe-orchestrator"]["props"]["apolloCache"]
-
+    try:
+        page_json = json.loads(
+            soup.select_one('script[type="application/discover+json"]').text
+        )["mfe-orchestrator"]["props"]["apolloCache"]
+    except:
+        return None
+    
     if not page_json:
         return None
 
@@ -34,61 +36,82 @@ def product_parser(html):
             ]
         ]
 
-    product_data["Photo URL"] = product_json["defaultImageUrl"].split("?")[0]
-    product_data["Price"] = product_json["price"]["actual"]
+    product_data["Photo URL"] = None
+    if  product_json["defaultImageUrl"]:
+        product_data["Photo URL"] = product_json["defaultImageUrl"].split("?")[0]
 
-    if soup.select_one("div.ddsweb-buybox__price.ddsweb-price__container p"):
-        product_data["Price_text"] = soup.select_one(
-            "div.ddsweb-buybox__price.ddsweb-price__container p"
-        ).text.replace("Â", "")
-    else:
-        product_data["Price_text"] = ""
+    product_data["Price"] = None
+    product_data["Price_text"] = None
+
+    try:
+        if product_json['price'].get('actual'):
+            product_data["Price"] = product_json["price"]["actual"]
+
+        if soup.select_one("div.ddsweb-buybox__price.ddsweb-price__container p"):
+            product_data["Price_text"] = soup.select_one(
+                "div.ddsweb-buybox__price.ddsweb-price__container p"
+            ).text.replace("Â", "")
+        else:
+            product_data["Price_text"] = ""
+    except:
+        ...
 
     product_data["Price before promotion"] = None
     product_data["Price before promotion_text"] = None
-    if product_json.get("promotions") and product_json["promotions"][0].get("__ref"):
+    try:
+        if product_json.get("promotions") and product_json["promotions"][0].get("__ref"):
 
-        promotion_json = page_json[product_json["promotions"][0].get("__ref")]
+            promotion_json = page_json[product_json["promotions"][0].get("__ref")]
 
-        if promotion_json['price'] and promotion_json['price'].get("beforeDiscount"):
-            product_data["Price before promotion"] = promotion_json["price"][
-                "beforeDiscount"
-            ]
-            offer_soup = soup.select_one('div[class*="value-bar__promo-text"] span').text.lower()
-            product_data["Price before promotion_text"] = (
-                offer_soup
-                .split("now")[0]
-                .replace("was", "")
-                .replace("â", "")
-                .strip()
-            )
-            if not product_data["Price_text"]:
-                product_data["Price_text"] = (
-                    offer_soup.split("now")[1]
-                    .replace('â','')
+            if promotion_json['price'] and promotion_json['price'].get("beforeDiscount"):
+                product_data["Price before promotion"] = promotion_json["price"][
+                    "beforeDiscount"
+                ]
+                offer_soup = soup.select_one('div[class*="value-bar__promo-text"] span').text.lower()
+                product_data["Price before promotion_text"] = (
+                    offer_soup
+                    .split("now")[0]
+                    .replace("was", "")
+                    .replace("â", "")
                     .strip()
-                    )
+                )
+                if not product_data["Price_text"]:
+                    product_data["Price_text"] = (
+                        offer_soup.split("now")[1]
+                        .replace('â','')
+                        .strip()
+                        )
+    except:
+        ...
 
-    if product_json['reviews({"count":10,"offset":0})'].get("stats"):
-        product_data["Star Rating"] = (
-            product_json['reviews({"count":10,"offset":0})']
-            .get("stats")
-            .get("overallRating")
-        )
-        product_data["Reviews"] = (
-            product_json['reviews({"count":10,"offset":0})']
-            .get("stats")
-            .get("noOfReviews")
-        )
+    product_data["Star Rating"] = None
+    product_data["Reviews"]  = None
+    try:
+        if product_json['reviews({"count":10,"offset":0})'].get("stats"):
+            product_data["Star Rating"] = (
+                product_json['reviews({"count":10,"offset":0})']
+                .get("stats")
+                .get("overallRating")
+            )
+            product_data["Reviews"] = (
+                product_json['reviews({"count":10,"offset":0})']
+                .get("stats")
+                .get("noOfReviews")
+            )
+    except:
+        ...
 
-    product_data["Codes"] = " | ".join(
-        [
-            "baseProductId : " + product_json["baseProductId"],
-            "gtin : " + product_json["gtin"],
-            "tpnb : " + product_json["tpnb"],
-            "tpnc : " + product_json["tpnc"],
-        ]
-    )
+    try:
+        product_data["Codes"] = " | ".join(
+            [
+                "baseProductId : " + product_json["baseProductId"],
+                "gtin : " + product_json["gtin"],
+                "tpnb : " + product_json["tpnb"],
+                "tpnc : " + product_json["tpnc"],
+            ]
+        )
+    except:
+        product_data["Codes"]  = None
 
     product_data["Availability"] = "N"
 
@@ -115,22 +138,28 @@ def product_parser(html):
         product_data["Is product is shipped by official seller"] = "Y"
 
 
-    product_data["category"] = " > ".join(
-        [
-            i.text
-            for i in soup.select('nav[aria-label="breadcrumb"] li a')
-            if i.text.strip()
-        ]
-    )
-    product_data["Category URL"] = soup.select('nav[aria-label="breadcrumb"] li a')[
-        -1
-    ].get("href")
+    if categories :=soup.select('nav[aria-label="breadcrumb"] li a'):
+        product_data["category"] = " > ".join(
+            [
+                i.text
+                for i in categories
+                if i.text.strip()
+            ]
+        )
+
+        product_data["Category URL"] = categories[
+            -1
+        ].get("href")
 
     product_data["Rich content presence"] = "Yes"
     product_data["Video presence"] = "No"
-    product_data["Gallery pictures"] = len(product_json["images"]["display"])
-    product_data["Gallery images URLs"] = " | ".join(
-        [img["default"]["url"] for img in product_json["images"]["display"]]
-    )
+    product_data["Gallery pictures"] = 0
+    product_data["Gallery images URLs"]  = None
+
+    if product_json["images"] and product_json["images"].get('display'):
+        product_data["Gallery pictures"] = len(product_json["images"]["display"])
+        product_data["Gallery images URLs"] = " | ".join(
+            [img["default"]["url"] for img in product_json["images"]["display"]]
+        )
 
     return product_data
