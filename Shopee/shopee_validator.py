939698fee10ce5d4f4e8f82f2156eee91172fef0
naimish33 <naimish.dobariya@techquaint.com>
2024-12-18 15:54:45 +0530

    Update : Shopee validator

--- Shopee/shopee_validator.py
+++ Shopee/shopee_validator.py
@@ -2,7 +2,7 @@ def validate_response(response_json : dict) -> bool:
     if (response_json.get('data') and response_json['data'].get('item')) or response_json.get('items'):
         return True
     
-    if response_json.get('error'):
+    if response_json.get('error') or response_json.get('message'):
         return False
     
     return False
