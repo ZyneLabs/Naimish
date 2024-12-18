c77c510b7c48816d2c328e1e74f5df414b691a0d
naimish33 <naimish.dobariya@techquaint.com>
2024-12-18 18:12:28 +0530

    Update : VRBO validator for location

--- VRBO/vrbo_validator.py
+++ VRBO/vrbo_validator.py
@@ -1,19 +1,17 @@
 def validate_response(response_json : dict | list) -> bool:
-
     if isinstance(response_json, list):
         return all(validate_response(item) for item in response_json)
     
     if isinstance(response_json, dict):
-        if response_json.get('data') and (
-            response_json['data'].get('propertySearch') or # For property search
-            response_json['data'].get('propertyOffers') or # For property offers
-            response_json['data'].get('offersContactHost') or  # For contact host
-            response_json['data'].get('propertyAvailabilityCalendars') or  # For property calendars
-            response_json['data'].get('reviewsOverview') or  # For property reviews
-            response_json['data'].get('recommendationsModule') or  # For property recommendations
-            response_json['data'] # I guess all the valid responses have this
-        ):
+        if  (response_json.get('extensions') and
+            response_json['extensions'].get('analytics') and
+            response_json['extensions']['analytics'][0].get('tealiumUtagData') and 
+            response_json['extensions']['analytics'][0]['tealiumUtagData'].get('Geo') and
+            response_json['extensions']['analytics'][0]['tealiumUtagData']['Geo']['country'].lower() == 'us' and
+            response_json.get('data') # I guess all the valid responses have this
+            ) :
             return True
+            
 
         if response_json.get('errors') or response_json.get('message'):
             return False
